71e3d6ac57f738bc125db834561d82634eeca12a
Jake Engel <jakeengel01@gmail.com>
2018-04-21 09:20:23 -0400

    testing SASS on prod

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 // import './App.scss';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h2>
+        Hi there
+        <p>Hello World</p>
+      </h2>
+    </div>
+  );
 };
 
 export default App;
